f1f4d64e645b71dd77bc119cf2b6b3d6e20e33a2
Nicholas Wagner <nick.wagner@openspace.ai>
2022-05-10 15:55:22 -0700

    wip

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,11 @@
-import {createUseStyles} from 'react-jss'
+import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
   '@global': {
     body: {
       margin: 0,
       padding: 0,
-    }
+    },
   },
   app: {
     margin: 0,
@@ -13,17 +13,12 @@ const useStyles = createUseStyles({
     backgroundColor: 'white',
     height: '100%',
   },
-  
 });
 
 function App() {
   const c = useStyles();
- 
-  return (
-    <div className={c.app}>
-      <p>Vite is alivel and kicking!</p>
-    </div>
-  )
+
+  return <div className={c.app}>bimsum!</div>;
 }
 
-export default App
+export default App;
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../docs/'
+    outDir: '../docs/',
   },
-  plugins: [tsconfigPaths(),react()]
-})
+  plugins: [tsconfigPaths(), react()],
+});
